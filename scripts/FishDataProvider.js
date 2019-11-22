c2380a820cda2a948e1bd44175f43730dd2d2ae9
onteriowright <onteriowright@yahoo.com>
2019-11-22 10:54:44 -0600

    Martins Aquarium/ current state before destruction

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,56 @@
+const fishCollection = [
+  {
+    name: "Lucky",
+    food: ["algae", "crustaceans"],
+    species: "Shark",
+    location: "America",
+    length: 8,
+    image: "assets/lucky.jpg"
+  },
+  {
+    name: "Maria",
+    food: ["algae", "chips"],
+    species: "Catfish",
+    location: "Thialand",
+    length: 4,
+    image: "assets/maria.jpg"
+  },
+  {
+    name: "Lil Jon",
+    food: ["cheese", "worms"],
+    species: "Goldfish",
+    location: "America",
+    length: 1,
+    image: "assets/lil-jon.jpg"
+  },
+  {
+    name: "Tammie",
+    food: ["coffee", "bacon"],
+    species: "Bass",
+    location: "Moroco",
+    length: 3,
+    image: "assets/tammie.jpg"
+  },
+  {
+    name: "Mike",
+    food: ["bread", "turkey"],
+    species: "Perch",
+    location: "Colombia",
+    length: 2,
+    image: "assets/mike.jpg"
+  },
+  {
+    name: "Elvis",
+    food: ["shrimp", "cookies"],
+    species: "Octopus",
+    location: "China",
+    length: 5,
+    image: "assets/elvis.jpg"
+  }
+];
+
+export const useFish = () => {
+  return fishCollection;
+};
+
+//Immutability: Our core data, raw data should be private and hidden and should not be changed
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 import initializeDetailButtonEvents from "./dialog.js";
 import addMouseOverEventListeners from "./highlight.js";
 import hightlightTips from "./tips.js";
+import { useFish } from "./FishDataProvider.js";
 
 initializeDetailButtonEvents();
 addMouseOverEventListeners();
 hightlightTips();
+const allOfFish = useFish();
+
+for (const fish of allOfFish) {
+  console.log(fish);
+}
