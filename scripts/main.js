3d9e0de15ca4413d13760e0186bf41b80cbe190e
onteriowright <onteriowright@yahoo.com>
2019-11-26 13:25:53 -0600

    refactored code/ removed 2 imported files from fish main.js that were already imported in fishlistcomponent

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,9 @@
-import { useFish } from "./Fish/FishDataProvider.js";
-import { useTips } from "./FishTips/TipDataProvider.js";
 import FishListComponent from "./Fish/FishList.js";
 import TipsListComponent from "./FishTips/TipList.js";
 import addMouseOverEventListeners from "./Highlights/Highlight.js";
 import initializeDetailButtonEvents from "./FishDialog/FishDialog.js";
 import highlightTips from "./FishTips/TipHighlight.js";
 
-useFish();
-useTips();
 FishListComponent();
 TipsListComponent();
 addMouseOverEventListeners();
